9dcfabf7f290b901ecd3ca5a619a408d79a84d9f
AiltonHrdz <ailton958hrdz@gmail.com>
2026-04-17 14:11:21 -0600

    navegacion SPA en pagina de inicio

--- frontend/src/app/components/inicio/inicio.component.ts
+++ frontend/src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // 1. Importamos la herramienta de navegación
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true, // Aseguramos que sea standalone
+  imports: [RouterLink], // 2. La agregamos a los imports del componente
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  // Aquí puedes agregar lógica más tarde, por ahora se queda limpio.
 }
